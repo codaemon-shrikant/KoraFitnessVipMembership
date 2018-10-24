324dea4370d802e7f3360ab6e76d915c53fece4d
codaemon-shrikant <shrikant.jadhav@codaemonsoftwares.com>
2018-10-24 18:26:15 +0530

    debuggin

--- ProcessOrderCreation.php
+++ ProcessOrderCreation.php
@@ -24,13 +24,13 @@ $orderDetails = json_decode(file_get_contents('php://input'));
     fwrite($file_handle, $orderId. " + ".$customerId." + ".$couponCode);
     fclose($file_handle);
 
-	//$data = $vipMembership->checkCoupon($orderId, $customerId, $status, $couponCode);//check coupon from db and webhook response
-/*
+	$data = $vipMembership->checkCoupon($orderId, $customerId, $status, $couponCode);//check coupon from db and webhook response
+
 
 $file_handle = fopen('my_filename.json', 'w');
 fwrite($file_handle, json_encode($data));
 fclose($file_handle);
-*/
+
 /*	$vipMembership->updateOrderIDinCoupon($data->id, $data->order_id) 
 
 	$creditUsed = $data->credit_used;//amount from coupon table
