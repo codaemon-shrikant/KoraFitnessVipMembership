bc122d2e1d997d6cf339742ce7ad1bc6bc494016
codaemon-shrikant <shrikant.jadhav@codaemonsoftwares.com>
2018-10-29 13:55:22 +0530

    Removed debbug testment

--- ProcessOrderCreation.php
+++ ProcessOrderCreation.php
@@ -20,11 +20,11 @@ $orderDetails = json_decode(file_get_contents('php://input'));
 	$couponCode = $orderDetails->discount_codes[0]->code;
 
 	$data = $vipMembership->checkCoupon($customerId, $couponCode);//check coupon from db and webhook response
-
+/*
 	$file_handle = fopen('my_filename.json', 'w');
     fwrite($file_handle, $orderId. " + ".$customerId." + ".$couponCode);
     fclose($file_handle);
-
+*/
 	$vipMembership->updateOrderIDinCoupon($data['id'], $orderId);
 
 	$creditUsed = $data['credit_used'];//amount from coupon table
